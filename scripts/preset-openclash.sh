40b9de89088ad66213dec34cbf4a1e826276bb7f
toringzhang <571796499@qq.com>
2023-05-06 01:54:50 +0800

    openclash

---
+++ scripts/preset-openclash.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget --no-check-certificate -O "clash-core-linux-armv8.tar.gz" https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-armv8.tar.gz
+tar -zxf clash-core-linux-armv8.tar.gz
+mv clush /etc/openclash/core/clash
+wget --no-check-certificate -O "clash-tun-linux-armv8.tar.gz" https://github.com/vernesong/OpenClash/releases/download/TUN/clash-linux-armv8.tar.gz
+tar -zxf clash-tun-linux-armv8.tar.gz
+mv clash /etc/openclash/core/clash_game
