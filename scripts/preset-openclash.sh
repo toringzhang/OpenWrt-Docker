75ba9bd07133dd633994e17586889f0a6316e7b6
toringzhangqian <toringzhangqian@didiglobal.com>
2023-05-06 16:10:48 +0800

    up to 21.02.5

--- scripts/preset-openclash.sh
+++ scripts/preset-openclash.sh
@@ -3,12 +3,12 @@
 mkdir -p files/root
 pushd files/root
 
-mkdir -p /etc/openclash/core/
-wget --no-check-certificate -O "clash-core-linux-armv8.tar.gz" https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-armv8.tar.gz
-tar -zxf clash-core-linux-armv8.tar.gz
-mv clash /etc/openclash/core/clash
-wget --no-check-certificate -O "clash-tun-linux-armv8.tar.gz" https://github.com/vernesong/OpenClash/releases/download/TUN/clash-linux-armv8.tar.gz
-tar -zxf clash-tun-linux-armv8.tar.gz
-mv clash /etc/openclash/core/clash_game
+sudo mkdir -p /etc/openclash/core/
+wget --no-check-certificate -O "clash-core-linux-armv8.tar.gz" https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-armv8.tar.gz && \
+tar -zxf clash-core-linux-armv8.tar.gz && \
+sudo mv ./clash /etc/openclash/core/clash && rm -f clash-core-linux-armv8.tar.gz
+wget --no-check-certificate -O "clash-tun-linux-armv8.tar.gz" https://github.com/vernesong/OpenClash/releases/download/TUN/clash-linux-armv8.tar.gz && \
+tar -zxf clash-tun-linux-armv8.tar.gz && \
+sudo mv ./clash /etc/openclash/core/clash_game && rm -f clash-tun-linux-armv8.tar.gz
 
 popd
