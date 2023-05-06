ceb5b8dfa0a94be2a5403cb962d9fdea6c5ffac0
toringzhangqian <toringzhangqian@didiglobal.com>
2023-05-06 14:35:31 +0800

    update

--- scripts/preset-ipv6.sh
+++ scripts/preset-ipv6.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+mkdir -p files/root
+pushd files/root
+
 wget --no-check-certificate -O "./ipv6-helper.sh" https://openwrt.cc/scripts/ipv6-helper.sh
 chmod +x ./ipv6-helper.sh
 
 ./ipv6-helper.sh install
+
+popd
--- scripts/preset-openclash.sh
+++ scripts/preset-openclash.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+mkdir -p files/root
+pushd files/root
+
 mkdir -p /etc/openclash/core/
 wget --no-check-certificate -O "clash-core-linux-armv8.tar.gz" https://github.com/vernesong/OpenClash/releases/download/Clash/clash-linux-armv8.tar.gz
 tar -zxf clash-core-linux-armv8.tar.gz
@@ -7,3 +10,5 @@ mv clash /etc/openclash/core/clash
 wget --no-check-certificate -O "clash-tun-linux-armv8.tar.gz" https://github.com/vernesong/OpenClash/releases/download/TUN/clash-linux-armv8.tar.gz
 tar -zxf clash-tun-linux-armv8.tar.gz
 mv clash /etc/openclash/core/clash_game
+
+popd
